75ad218789ec233d941ab281e83a34516a124f3e
Tiryoh <tiryoh@gmail.com>
2020-06-07 16:19:09 +0900

    fix: colcon build error

--- dashing/prepare_release.sh
+++ dashing/prepare_release.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eu
 
-cd /ros_ws/src
+cd /ros_ws
 rosdep update
 sudo apt update
-rosdep install -r -y -i --from-paths .
+rosdep install -r -y -i --from-paths src
 colcon build
 echo "colcon build passed"
 git config --global user.name "${GIT_NAME}"
