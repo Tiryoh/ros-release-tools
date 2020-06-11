97e32c2c6c9bc28e165c6fd43ebc4d41d0c73567
Tiryoh <tiryoh@gmail.com>
2020-06-11 09:22:21 +0900

    feat: Add --ignore-src option to release_binary.sh

--- melodic/release_binary.sh
+++ melodic/release_binary.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
+
+source /ros_ws/install/setup.bash
+
 set -eu
 
 if [[ -e debian ]]; then rm -rf debian; fi
-bloom-generate rosdebian --os-name ubuntu --os-version bionic --ros-distro melodic
+
+if [[ $# -gt 0 ]] && [[ "$1" == "--ignore-src" ]]; then
+	touch /ros_ws/rosdep.yaml
+	sudo touch /etc/ros/rosdep/sources.list.d/50-my-packages.list
+	echo yaml file:///ros_ws/rosdep.yaml | sudo tee -a /etc/ros/rosdep/sources.list.d/50-my-packages.list > /dev/null
+	for ignore_package in ${@:2}; do
+		echo ignore: ${ignore_package}
+		echo "${ignore_package}:" >> /ros_ws/rosdep.yaml
+		echo "  ubuntu: [ros-melodic-$(echo ${ignore_package} | sed -e 's/_/-/g')]" >> /ros_ws/rosdep.yaml
+	done
+	rosdep update
+	bloom-generate rosdebian --os-name ubuntu --os-version bionic --ros-distro melodic
+else
+	bloom-generate rosdebian --os-name ubuntu --os-version bionic --ros-distro melodic
+fi
+
 fakeroot debian/rules binary
 mkdir -p release
 cp `ls ../*deb` ./release/
+echo "SUCCESS"
